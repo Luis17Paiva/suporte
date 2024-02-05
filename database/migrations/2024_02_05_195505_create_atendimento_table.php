98ee5cef8cf22a86d550c1ba0001d769592973b4
Luis Paiva <Luis17Paiva@gmail.com>
2024-02-05 19:56:06 -0300

    modified:   composer.lock renamed:    database/migrations/2024_02_05_115538_create_atendimento_table.php -> database/migrations/2024_02_05_195505_create_atendimento_table.php

--- database/migrations/2024_02_05_195505_create_atendimento_table.php
+++ database/migrations/2024_02_05_195505_create_atendimento_table.php
@@ -4,12 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAtendimentosTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
@@ -31,8 +29,8 @@ class CreateAtendimentosTable extends Migration
             // Chave estrangeira para a tabela de ramais (opcional)
             // $table->foreign('id_ramal')->references('id')->on('ramais')->onDelete('set null');
 
-            // Se desejar, você pode adicionar um índice para a coluna 'status'
-            // $table->index('status');
+          
+            $table->index('status');
         });
     }
 
@@ -45,4 +43,4 @@ class CreateAtendimentosTable extends Migration
     {
         Schema::dropIfExists('atendimento');
     }
-}
+};
